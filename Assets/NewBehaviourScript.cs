be081c942a428948a591620823fa6d998e3814a7
ChrisHaleyDev <chris.haley.fda@gmail.com>
2019-11-14 20:21:09 +0000

    WindmillPhysics

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float speed;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = Vector3.forward * speed;
+    }
+}
