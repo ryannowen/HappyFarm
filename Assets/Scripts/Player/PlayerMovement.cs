8d5c5f06984c1b43571df351b36a9fca4ec0946b
Justin <jgrigas@elam.co.uk>
2019-11-14 20:52:47 +0000

    Added jumping

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,12 +5,15 @@ namespace Player
 {
     public class PlayerMovement : MonoBehaviour
     {
-        [SerializeField] private KeyCode _forward;
-        [SerializeField] private KeyCode _backwards;
-        [SerializeField] private KeyCode _left;
-        [SerializeField] private KeyCode _right;
-        [SerializeField] private float _speed;
+        [SerializeField] private KeyCode _forwardKey = KeyCode.W;
+        [SerializeField] private KeyCode _backwardsKey = KeyCode.S;
+        [SerializeField] private KeyCode _leftKey = KeyCode.A;
+        [SerializeField] private KeyCode _rightKey = KeyCode.D;
+        [SerializeField] private KeyCode _jumpKey = KeyCode.Space;
+        [SerializeField] private float _speed, _jumpForce;
+        [SerializeField] private int _maxJumps;
         [SerializeField] private Transform _cameraTransform;
+        private int _jumpCount;
         private Rigidbody _rb;
 
         private void Awake()
@@ -25,27 +28,42 @@ namespace Player
             var right = _cameraTransform.right;
             var cameraForward = new Vector3(forward.x, 0, forward.z);
             var cameraRight = new Vector3(right.x, 0, right.z);
-            // Get input.
+            // Get movement input.
             var moveForce = Vector3.zero;
-            if (Input.GetKey(_forward))
+            if (Input.GetKey(_forwardKey))
             {
                 moveForce += cameraForward;
             }
-            else if (Input.GetKey(_backwards))
+            else if (Input.GetKey(_backwardsKey))
             {
                 moveForce -= cameraForward;
             }
 
-            if (Input.GetKey(_right))
+            if (Input.GetKey(_rightKey))
             {
                 moveForce += cameraRight;
             }
-            else if (Input.GetKey(_left))
+            else if (Input.GetKey(_leftKey))
             {
                 moveForce -= cameraRight;
             }
+            // Jumping.
+            if (Input.GetKeyDown(_jumpKey) && _jumpCount < _maxJumps)
+            {
+                _jumpCount++;
+                // Reset the Y velocity.
+                var velocity = _rb.velocity;
+                velocity = new Vector3(velocity.x, 0, velocity.z);
+                _rb.velocity = velocity;
+                _rb.AddForce(Vector3.up * _jumpForce);
+            }
             // Move the player.
             _rb.MovePosition(transform.position + moveForce * _speed);
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            _jumpCount = 0;
         }
     }
 }
