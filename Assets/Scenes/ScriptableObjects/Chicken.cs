55d3e11909a58c5eba5d67620743fb6860841d17
Cayleb Lucas <Q5082835@live.tees.ac.uk>
2019-11-14 19:49:58 +0000

    Added base AI for Chicken

---
+++ Assets/Scenes/ScriptableObjects/Chicken.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(Rigidbody))]
+public class Chicken : MonoBehaviour
+{
+    public float WonderRadius;
+    public float WonderTimer;
+    public float MovementSpeed;
+
+    private NavMeshAgent Agent;
+    private float Timer;
+
+    void Start()
+    {
+        Agent = GetComponent<NavMeshAgent>();
+        Timer = WonderTimer;
+    }
+
+    void Update()
+    {
+        //Below Line only used for testing at Run time.
+        Agent.speed = MovementSpeed;
+
+        Timer += Time.deltaTime;
+        if (Timer >= WonderTimer)
+        {
+            Vector3 newPos = RandomNavSphere(transform.position, WonderRadius, -1);
+            Agent.SetDestination(newPos);
+            Timer = 0;
+        }
+    }
+
+    Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask)
+    {
+        Vector3 randDirection = Random.insideUnitSphere * dist;
+
+        randDirection += origin;
+
+        NavMeshHit navHit;
+
+        NavMesh.SamplePosition(randDirection, out navHit, dist, layermask);
+
+        return navHit.position;
+    }
+}
